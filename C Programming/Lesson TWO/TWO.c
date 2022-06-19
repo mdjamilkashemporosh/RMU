e208e766cfa8086e04f6338be0173d829a8229df
porosh2002 <jamilkashem2002@gmail.com>
2022-06-19 20:04:17 +0600

    first commit

---
+++ C Programming/Lesson TWO/TWO.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // declare variables । ভেরিয়েবল নেওয়া।
+    int num, i, sum = 0;
+    printf(" Enter a positive number: ");
+    // take any positive number | আপনি যে কোনো পজিটিভ/ধনাত্মক নম্বর বেছে নিতে পারেন।
+    scanf("%d", & num);
+    // executes until the condition remains true. | শর্তটি সত্য হওয়া পর্যন্ত এই loop কার্যকর হবে। 
+    for (i = 0; i <= num; i++) {
+        // at each iteration the value of i is added to the sum variable | প্রতিটি পুনরাবৃত্তিতে, i-এর মানের সাথে sum এর বর্তমান মান যোগ করা হচ্ছে, এবং যোগফলকে  sum এর নতুন মান নির্ধারণ করা হচ্ছে। 
+        sum = sum + i*i;
+    }
+    // display the sum of number ।   sum  এর সর্বশেষ মান প্রিন্ট করা হচ্ছে। 
+    printf("\n Sum of the first %d number is: %d", num, sum);
+    return 0;
+}
---
+++ C Programming/README.md
@@ -0,0 +1,10 @@
+# Getting Started with C Programming LESSON 1 | সি প্রোগ্রামিং পাঠ 1 
+
+You must have Codeblocks Software or something like that in order to run this file. [Codeblocks Software](https://github.com/facebook/create-react-app). | 
+
+এই ফাইলটি চালানোর জন্য আপনার অবশ্যই Codeblocks সফটওয়্যার বা এ ধরনের কিছু থাকতে হবে। [কোডব্লকস সফটওয়্যার](https://github.com/facebook/create-react-app). |
+
+## Others options |  বিকল্প
+
+You can use web based compiler like [Programiz](https://www.programiz.com/). | আপনি [প্রোগ্রামিজ](https://www.programiz.com/) এর মত ওয়েব ভিত্তিক কম্পাইলার ব্যবহার করতে পারেন । 
+
